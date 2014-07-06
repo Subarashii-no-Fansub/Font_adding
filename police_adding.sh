a8978ffbff21877cf9b8535248f5a1c5485ae33f
Subarashii no Fansub <subarashii-no-fansub@null.net>
2014-07-06 23:37:48 +0200

    problème lors des espaces
    (toujours ><) 
    mais corrige #1

--- police_adding.sh
+++ police_adding.sh
@@ -13,16 +13,15 @@ read -p "Numéro des sous-titres : " var_idss
 
 nombrearg="$#"
 
-for ((i=2 ; $nombrearg-$i ; i++))
+for ((i=2 ; $nombrearg+1-$i ; i++))
 do
-  fullfilename=$(basename "$i")
-  filename="${fullfilename%.*}"
-  attachemine=$(file --brief --mime $i)
-  command="$command --attachment-mime-type $attachemine  --attachment-name $filename --attach-file $i"
+	fullfilename=$(basename "${!i}")
+	attachemine=$(file --brief --mime-type "${!i}")
+	command="$command --attachment-mime-type $attachemine  --attachment-name $fullfilename --attach-file ${!i}"
 done
 
 fullfilename=$(basename "$1")
 
-mkvmerge -o "$fullfilename" --default-track $var_idvid:yes --forced-track $var_idvid:no --language $var_idson:jpn --default-track $var_idson:yes --forced-track $var_idson:no --language $var_idss:fre --default-track $var_idss:yes --forced-track $var_idss:no --video-tracks $var_idvid --audio-tracks $var_idson --subtitle-tracks $var_idss --no-track-tags --no-global-tags "$1" $command
+mkvmerge -o "final $fullfilename" --default-track $var_idvid:yes --forced-track $var_idvid:no --language $var_idson:jpn --default-track $var_idson:yes --forced-track $var_idson:no --language $var_idss:fre --default-track $var_idss:yes --forced-track $var_idss:no --video-tracks $var_idvid --audio-tracks $var_idson --subtitle-tracks $var_idss --no-track-tags --no-global-tags "$1" $command
 
 echo "FINI !"
