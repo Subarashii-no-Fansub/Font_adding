2aa77d4b1f902191737d02bc4c08e2eb8cc2ee37
Subarashii no Fansub <subarashii-no-fansub@null.net>
2014-07-10 23:24:12 +0200

    Merci à OmegaPhil pour son aide
    Fix #2

--- police_adding.sh
+++ police_adding.sh
@@ -19,13 +19,13 @@ for ((i=2 ; $nombrearg+1-$i ; i++))
 do
 	fullfilename=$(basename "${!i}")
 	attachemine=$(file --brief --mime-type "${!i}")
-	commande=$(printf "%s --attachment-mime-type \"%s\"  --attachment-name \"%s\" --attach-file \"%s\"" "$commande" "$attachemine" "$fullfilename" "${!i}")
+	commande=$(printf "%s --attachment-name \"%s\"  --attachment-mime-type \"%s\" --add-attachment \"%s\"" "$commande" "$fullfilename" "$attachemine" "${!i}")
 done
 
 fullfilename=$(basename "$1")
 
 echo $commande
 
-mkvmerge -o "final $fullfilename" --default-track $var_idvid:yes --forced-track $var_idvid:no --language $var_idson:jpn --default-track $var_idson:yes --forced-track $var_idson:no --language $var_idss:fre --default-track $var_idss:yes --forced-track $var_idss:no --video-tracks $var_idvid --audio-tracks $var_idson --subtitle-tracks $var_idss --no-track-tags --no-global-tags  $commande "$1"
+mkvpropedit "$1" $($commande)
 
 echo "FINI !"
